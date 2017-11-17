7ba0d07695c5655ecce0c8e1e1647033f0ed2986
stevenmsth <swm1521@jagmail.southalabama.edu>
2017-11-16 18:12:55 -0600

    Clean up.

--- app/src/main/java/com/csc_331_jagwares/bluetoothattendee/fragments/TakeAttendanceFragment.java
+++ app/src/main/java/com/csc_331_jagwares/bluetoothattendee/fragments/TakeAttendanceFragment.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
